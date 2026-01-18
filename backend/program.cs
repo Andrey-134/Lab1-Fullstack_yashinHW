1f5a13331c963c335cea6c23cc2a7daca75b1cc0
Андрей-134 <aasin2753@gmail.com>
2026-01-18 15:14:27 +0300

    логика продолжения

--- backend/program.cs
+++ backend/program.cs
@@ -2,3 +2,10 @@
 string name=Console.ReadLine();
 Console.WriteLine($"Привет, {name}!");
 Console.WriteLine($"Сегодня {DateTime.Now.DayOfWeek}");
+Console.Write("Хотите продолжить? (y/n): ");
+string answer = Console.ReadLine()?.Trim().ToLower();
+
+if (answer == "y" || answer == "yes")
+    Console.WriteLine("Отлично!");
+else
+    Console.WriteLine("До свидания!");
